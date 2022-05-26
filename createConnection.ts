444a149e21a440e1c6b21376b632858654448a9d
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2022-05-26 19:13:24 +0530

    changed name of environment variable
    actually the heroku server saves it as DATABASE_URL so we shld also in the same way in the code

--- createConnection.ts
+++ createConnection.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 createConnection(
     {
-        url : process.env.DATABASE,
+        url : process.env.DATABASE_URL,
         entities : [hotels],
         type : "postgres",
         extra : {ssl : {rejectUnauthorised : false}}
