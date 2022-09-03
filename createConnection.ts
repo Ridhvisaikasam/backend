b22408f32ec76255985bfdeaffb1dde8977c84c3
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2022-09-03 21:29:24 +0530

    changed config of connection and PGSSL

--- createConnection.ts
+++ createConnection.ts
@@ -4,11 +4,17 @@ import hotels from "./hotels";
 
 dotenv.config();
 
-createConnection(
-    {
-        url : process.env.DATABASE_URL,
-        entities : [hotels],
-        type : "postgres",
-        extra : {ssl : {rejectUnauthorised : false}}
-    }
-)
+const conn = createConnection(
+    { 
+        name: process.env.NAME, 
+        type: "postgres", 
+        host: process.env.HOST, 
+        port: 5432, 
+        username: process.env.USERNAME, 
+        password: process.env.PASSWORD, 
+        database: process.env.DATABASE ,
+        entities: [hotels]
+     }
+);
+
+console.log(conn);
