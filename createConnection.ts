904db50ab890983bf5681c62a0275ef87dba7e45
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2022-09-04 09:27:32 +0530

    reverted the configuration of connection

--- createConnection.ts
+++ createConnection.ts
@@ -4,17 +4,39 @@ import hotels from "./hotels";
 
 dotenv.config();
 
-const conn = createConnection(
+createConnection({
+    url: process.env.DATABASE_URL,
+    entities: [hotels],
+    type:"postgres",
+    extra:{
+        ssl:{
+            rejectUnauthorized: false
+        }
+    }
+})
+
+
+
+/*
+the only error was .env file PGSSLMODE
+
+this change created extra error dont use this
+
+const conn = async ()=>{
+    const connect= await createConnection(
     { 
-        name: process.env.NAME, 
+        name: process.env.DATABASE_NAME, 
         type: "postgres", 
-        host: process.env.HOST, 
+        url:process.env.DATABASE_URL,
+        host: process.env.DATABASE_HOST, 
         port: 5432, 
-        username: process.env.USERNAME, 
-        password: process.env.PASSWORD, 
-        database: process.env.DATABASE ,
-        entities: [hotels]
+        username: process.env.DATABASE_USERNAME, 
+        password: process.env.DATABASE_PASSWORD, 
+        database: process.env.DATABASE_DATABASE ,
+        entities: [hotels],
+        extra : {ssl : {rejectUnauthorised : false}}
      }
-);
+)}
 
 console.log(conn);
+*/
